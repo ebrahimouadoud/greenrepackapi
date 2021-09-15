47aeaaf2f2f0ecacc622274e88fe9f1574ac3966
eouadoud <ebr.ouadoud@gmail.com>
2021-09-15 12:44:39 +0200

    user manager

--- Controller/user-manager.controller.js
+++ Controller/user-manager.controller.js
@@ -2,17 +2,33 @@ const authService = require("../authmiddelwares/AuthService");
 const User = require("../models/user.model")
 const Role = require("../models/role.model")
 const db = require("../models")
+var bcrypt = require("bcryptjs");
 
 exports.getAllUsers = (req, res) => {
-    // if(authService.isAdmin){
-    //     db.user.findAll().then( users => { 
-    //         res.status(400).send({
-    //             rows: users
-    //           });
-    //       })
-    // }
+    if(authService.isAdmin){
+        db.user.findAll().then( users => { 
+            users.forEach(user => {
+                if( user.getRoles()[0] == "admin" ){
+                    users.splice(users.findIndex(us => user.id == us.id ), 1 )
+                }
+            });
+            })
+    }
+    if(authService.isManager){
+        db.user.findAll().then( users => {
+            users.forEach(user => {
+                if( user.getRoles()[0] == "admin" ){
+                    users.splice(users.findIndex(us => user.id == us.id ), 1 )
+                }
+            });
+            res.status(200).send({
+                rows: users
+                });
+            })
+    }
 
 };
+
 
 exports.getUserById = (req, res) => {
     db.user.findOne({
@@ -36,10 +52,25 @@ exports.getUserById = (req, res) => {
 
 exports.createUser = (req, res) => {
     try {
-        const users = db.user.create(req.body);
-        return res.status(201).json({
-            users,
-        });
+        db.user.create(
+            {
+                lastname: req.body.lastname,
+                firstname: req.body.firstname,
+                username: req.body.username,
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 8),
+                status: "Active",
+                telephone: req.body.telephone,
+            }).then(user => {
+                let role_id = db.role.findOne({
+                    where: {name: req.body.role}
+                })
+                user.setRoles([role_id])
+                return res.status(201).json({
+                    "user": user
+                });
+            }) ;
+        
     } catch (error) {
         return res.status(500).json({ error: error.message })
     }
@@ -72,20 +103,27 @@ exports.updateUser = (req, res) => {
 
 
 exports.toggleActivation = (req, res) => {
-
-    db.user.update(
-              { status: req.body.status},
-              { where: {id: req.params.id} }
-         ).then(users => {
-            res.status(200).send({
-                message: "User Status was updated successfully."
+    db.user.findOne({
+        where: {
+          id: req.params.id
+        }
+      }).then(user => {
+          let ns = "Blocked"
+          if(user.status == "Blocked"){
+              ns = "Active"
+          }
+          user.update(
+              {status: ns }
+          ).then( us => {
+                res.status(200).send({
+                    message: "User Status was updated successfully."
+                });
+          }).catch(err => {
+                res.status(500).send({
+                    message: "Error updating User Status with id=" + id
+                });
             });
-         })
-         .catch(err => {
-            res.status(500).send({
-                message: "Error updating User Status with id=" + id
-            });
-        });
+      })
 }
 
 exports.deleteUserById = (req, res) => {
