e21baf54f295b2cd0972577eada054407eaf0e59
eouadoud <ebr.ouadoud@gmail.com>
2021-09-23 02:25:37 +0200

    Vérification du numéro RNA

--- Controller/demande.controller.js
+++ Controller/demande.controller.js
@@ -1,9 +1,47 @@
 const db = require('../models')
  const InsciAssociation = db.inscriptionAssociation;
-
-
+ var express = require('express');
+ var router = express.Router();
+ const https = require('https');
 // POST >> Create Demande d insciption (Association)
 exports.createDemande = (req, res) => {
+    //
+    https.get('https://entreprise.data.gouv.fr/api/rna/v1/id/' + req.body.RNA, (resp) => {
+      let data = '';
+
+      // A chunk of data has been received.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+      //console.log(' resp ', resp)
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        console.log(data);
+        console.log('resp')
+        console.log(resp.statusCode)
+        if(resp.statusCode == 404){
+          return res.status(404).json({ error: "RNA non trouvé" })
+        }else if(resp.statusCode == 200){
+          InsciAssociation.create({
+            name: req.body.name,
+            description: req.body.description,
+            adresse: req.body.adresse,
+            RNA: req.body.RNA,
+            mail: req.body.mail
+          }).then((result) => {
+            return res.status(200).json({
+                "result": result,
+              })
+          }).catch((error) => {
+            return res.status(500).json({ error: error.message })
+          });
+        }
+      });
+
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+    /*
     InsciAssociation.create({
         name: req.body.name,
         description: req.body.description,
@@ -16,5 +54,5 @@ exports.createDemande = (req, res) => {
           })
       }).catch((error) => {
         return res.status(500).json({ error: error.message })
-      });
+      });*/
 }
