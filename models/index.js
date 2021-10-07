27bab1c202e6f3a1fdf7049c4d0564449d9de225
ebrahimouadoud <ebrahim.ouadoud.d@gmail.com>
2021-10-08 01:08:44 +0200

    desable loggin

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
     host: config.HOST,
     dialect: config.DIALECT,
     port: config.PORT,
-    logging: true,
+    logging: false,
     pool: {
       max: 5,
       min: 0,
